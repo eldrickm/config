b32637a1f039827627bd12ca93f31209b69ab1df
Eldrick Millare <eldrick@stanford.edu>
2021-01-23 00:43:11 -0800

    Attempts to add vim8 to config.sh

--- config.sh
+++ config.sh
@@ -1,28 +1,34 @@
-# Installs Vim
-sudo apt-get install vim
+# Requirements
+# - git
+# - vim 8
 
-# Installs Git
-sudo apt-get install git
+# Installs zsh
+sudo apt-get install zsh
 
-# Installs Vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Copy over zsh settings
+cp .zshrc ~/.zshrc
 
 # Copy /config/.vimrc to ~/
 cp .vimrc ~/.vimrc
 
-# Copy /config/.bashrc to ~/
-cp .bashrc ~/.bashrc
+# Install Vim Packages
+# Install gruvbox theme
+git clone https://github.com/morhetz/gruvbox.git ~/.vim/pack/default/start/gruvbox
 
-# Install Vundle Plugins
-vim +PluginInstall +qall
+# Install nerdcommenter
+git clone https://github.com/preservim/nerdcommenter.git ~/.vim/pack/default/start/nerdcommenter
 
-# Install Powerline Fonts for vim airline
-wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
-wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
-mkdir ~/.local/share/fonts
-mv PowerlineSymbols.otf ~/.local/share/fonts/
-fc-cache -vf ~/.local/share/fonts/
-mkdir ~/.config/fontconfig/
-mkdir ~/.config/fontconfig/conf.d/
-mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+# Install vim-autoformat
+git clone https://github.com/Chiel92/vim-autoformat.git ~/.vim/pack/default/start/vim-autoformat
 
+# Install vim-indent-guides
+git clone https://github.com/nathanaelkane/vim-indent-guides ~/.vim/pack/default/start/vim-indent-guides
+
+# Install YouCompleteMe
+sudo apt install build-essential cmake vim-nox python3-dev
+sudo apt install mono-complete golang nodejs default-jdk npm
+git clone https://github.com/Valloric/YouCompleteMe ~/.vim/pack/default/start/YouCompleteMe
+git submodule update --init --recursive && ./install.py --all
